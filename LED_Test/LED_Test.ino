be6a37aef5c53d2110cff9b9f232433c872fd0c9
AdamSchwaatz <adamschwaatz@gmail.com>
2023-04-14 16:26:33 -0400

    Continue to test old LEDs

--- LED_Test/LED_Test.ino
+++ LED_Test/LED_Test.ino
@@ -1,34 +1,46 @@
 #include <WS2812B_LED_Strip.h>
 
-WS2812B_LED_Strip WS2812B(3);
+WS2812B_LED_Strip WS2812B(5);
 byte r = 0;
 byte g = 30;
 byte b = 70;
+boolean flip = false;
 
 void setup() {
   Serial.begin(115200);
   //WS2812B.clearLEDs(1,true);
-  delay(500);
+  //delay(500);
+  pinMode(3,OUTPUT);
 }
 
 void loop() {
-  if(r==255){
-    r=0;
+  // if(r==255){
+  //   r=0;
+  // }else{
+  //   r++;
+  // }
+  // if(g==255){
+  //   g=0;
+  // }else{
+  //   g++;
+  // }
+  // if(b==255){
+  //   b=0;
+  // }else{
+  //   b++;
+  // }
+  r=255;
+  g=255;
+  b=255;
+  if(flip){
+    flip = false;
+    digitalWrite(4,HIGH);
   }else{
-    r++;
-  }
-  if(g==255){
-    g=0;
-  }else{
-    g++;
-  }
-  if(b==255){
-    b=0;
-  }else{
-    b++;
+    flip = true;
+    digitalWrite(4,LOW);
   }
   WS2812B.color(g,r,b);
-  delayMicroseconds(100);
+  delayMicroseconds(50);
   // Serial.print("Sending: ");
   // Serial.print(r);
   // Serial.print(" ");
